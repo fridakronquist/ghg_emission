ed5d5ef083d6988daf7d9a9c98790096a709c578
Frida Kronquist <fridakronquist@gmial.com>
2023-10-19 08:42:08 +1100

    Performed linear regression and saved plots of potential models

---
+++ code/part1_ghg_trend.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+
+source("code/data_preparation.R")
+
+# Group by year and calculate total ghg emission per year.
+ghg_tot <- ghg_long %>%
+  group_by(year) %>%
+  drop_na(ghg_emission) %>% # droppes rows with NA
+  summarise(total_ghg = sum(ghg_emission))
+
+# Linear Model: model_lin: total_ghg~year
+model_lin <- lm(total_ghg~year, data=ghg_tot)
+summary(model_lin)
+
+# Basic Model Plots: model_lin
+png("figures/part1/model_lin_plots.png",
+    width = 800, height = 800)
+par(mfrow=c(2,2))
+plot(model_lin)
+dev.off()
+
+# Scatter plot model_lin
+ggplot(ghg_tot, aes(x=year, y=total_ghg)) +
+  geom_point()
+ggsave("figures/part1/model_lin_scatter.png", width = 6, height = 4)
+
+# Try	to	find	a	better	fit
+
+# Linear Model: model_log: log10(total_ghg)~year
+model_log <- lm(log10(total_ghg)~year, data=ghg_tot)
+summary(model_log)
+
+# Basic Model Plots: model_log
+png("figures/part1/model_log_plots.png",
+    width = 800, height = 800)
+par(mfrow=c(2,2)) 
+plot(model_log)
+dev.off()
+
+# Scatter plot model_log
+ggplot(ghg_tot, aes(x=year, y=log10(total_ghg))) +
+  geom_point()
+ggsave("figures/part1/model_log_scatter.png", width = 6, height = 4)
+
+# Linear Model: model_sqrt: sqrt(total_ghg)~year
+model_sqrt <- lm(sqrt(total_ghg)~year, data=ghg_tot)
+summary(model_sqrt)
+
+# Basic Model Plots: model_sqrt
+png("figures/part1/model_sqrt_plots.png",
+    width = 800, height = 800)
+par(mfrow=c(2,2))
+plot(model_sqrt)
+dev.off()
+
+# Scatter plot model_sqrt
+ggplot(ghg_tot, aes(x=year, y=sqrt(total_ghg))) +
+  geom_point()
+ggsave("figures/part1/model_sqrt_scatter.png", width = 6, height = 4)
+
+
+
+
+
+
