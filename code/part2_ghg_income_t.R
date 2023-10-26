662e462a2872c0c710def4df6e858e1009d34aae
Frida Kronquist <fridakronquist@gmial.com>
2023-10-26 15:25:34 +1100

    Changed boxplot part 2 to be same figure

--- code/part2_ghg_income_t.R
+++ code/part2_ghg_income_t.R
@@ -39,3 +39,46 @@ wilcox.test(ghg_emission ~ income_group, alternative="two.sided", data=ghg_incom
 # Numerical summary
 numSummary(ghg_income_t[,"ghg_emission", drop=FALSE], groups=ghg_income_t$income_group,
            statistics=c("mean", "sd", "quantiles"), quantiles=c(0, .25, .5, .75, 1))
+
+
+# Plot boxplot
+plot_box_outliers <- ggplot(ghg_income_t, aes(x=reorder(income_group, ghg_emission), y=ghg_emission, color=income_group)) + 
+  geom_boxplot() + 
+  labs(title = "Greenhouse Gas Emission 2019",
+       subtitle = "For high income and low income countries",
+       y = "GHG emission (kt)", 
+       x = "Income group") +
+  theme_bw() + 
+  theme(plot.title = element_text(face="bold", size=18),
+        plot.subtitle = element_text(size=15),
+        axis.title.x = element_text(size=16),
+        axis.title.y = element_text(size=16),
+        axis.text.x = element_text(size=12),
+        axis.text.y = element_text(size=12), 
+        legend.position = "none") +
+  scale_color_manual(values=c("#5D5292","#84B0A5")) +
+  coord_flip() +
+  annotate("text", x = 1.9, y = 6001210, label = "USA") +
+  annotate("text", x = 1.9, y = 1166510, label = "JPN")
+
+# Same plot without outliers
+plot_box_nooutliers <- ggplot(ghg_income_t, aes(x=reorder(income_group, ghg_emission), y=ghg_emission, color=income_group)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  labs(title = " ",
+       y = "GHG emission (kt)", 
+       x = "Income group") +
+  theme_bw() + 
+  theme(plot.title = element_text(face="bold", size=18),
+        plot.subtitle = element_text(size=15),
+        axis.title.x = element_text(size=16),
+        axis.title.y = element_text(size=16),
+        axis.text.x = element_text(size=12),
+        axis.text.y = element_text(size=12),
+        legend.position = "none") +
+  scale_color_manual(values=c("#5D5292","#84B0A5")) +
+  coord_flip(ylim=quantile(ghg_income_t$ghg_emission, c(0.1, 0.9))) +
+  annotate("text", x = 1.9, y = 6001210, label = "USA") +
+  annotate("text", x = 1.9, y = 1166510, label = "JPN")
+
+# both box plots in same figure
+plot_grid(plot_box_outliers, plot_box_nooutliers, ncol = 1, nrow = 2)
